8025dd4eac98f3a739a1e5b719e7aac75b471f0d
binoysb007 <43755099+binoysb007@users.noreply.github.com>
2021-07-21 14:56:47 +0100

    Removed the unused namespace

--- StackOverflowTest/Pages/TagSearchPage.cs
+++ StackOverflowTest/Pages/TagSearchPage.cs
@@ -1,12 +1,7 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 using StackOverflowAutoFramework.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace StackOverflowTest.Pages
 {
